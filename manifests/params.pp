5766aad7ade9a807478cc5f023163396e4b11a02
b4ldr <github@johnbond.org>
2016-10-24 16:01:49 +0100

    add params file

---
+++ manifests/params.pp
@@ -0,0 +1,12 @@
+# == Class: logstash_forwarder::params
+#
+class logstash_forwarder::params {
+  $conf_file = $::kernel ? {
+    default   => '/etc/logstash-forwarder.conf',
+    'FreeBSD' => '/usr/local/etc/logstash-forwarder.conf',
+  }
+  $logstash_cert_dir = $::kernel ? {
+    default   => '/etc/ssl/certs',
+    'FreeBSD' => '/etc/ssl',
+  } 
+}
